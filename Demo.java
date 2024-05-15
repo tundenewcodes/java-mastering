e8c41218bf26cfe400eb895c2610563a2226d65e
Tunde <fadipe11@gmil.com>
2024-05-16 00:49:37 +0100

    dsa

--- Demo.java
+++ Demo.java
@@ -1,9 +1,10 @@
 public class Demo {
     public static void main(String[] args) {
         int nums[] = {1, 3,5,7,9,11,13,15};
-        int target = 11;
+        int target = 13;
 
-        int result = linearSearch(nums, target);
+        int result = binarySearch(nums, target);
+        int result2 = linearSearch(nums, target);
         if(result == -1) {
             System.out.println("Not found");
         }
@@ -14,9 +15,11 @@ public class Demo {
 
     public static int linearSearch(int[] nums, int target) {
 
-
+        int steps = 0;
         for(int i = 0; i < nums.length; i++) {
+            steps++;
             if(nums[i] == target) {
+                System.out.println("Linear Steps: " + steps);
                 return i;
             }
         }
@@ -25,25 +28,26 @@ public class Demo {
 
 
     public static int binarySearch(int[] nums, int target) {
+        int steps = 0;
+        int left = 0;
+        int right = nums.length - 1;
 
-   int left = 0;
-   int right = nums.length - 1;
-
-   while(left <= right) {
-    int mid = (left + right) / 2;
-    if(nums[mid] == target) {
+        while(left <= right) {
+        int mid = (left + right) / 2;
+        if(nums[mid] == target) {
+        System.out.println("Binary Steps: " + steps);
         return mid;
-    }
-    else if(nums[mid] < target) {
+         }
+         else if(nums[mid] < target) {
         left = mid + 1;
-    }
-    else if(nums[mid] > target) {
+         }
+         else if(nums[mid] > target) {
         right = mid - 1;
-    }
+         }
 
-   }
-
-
+   };
+        System.out.println("Steps: " + steps );
+    return -1;
        
-    }
+    };
 }
